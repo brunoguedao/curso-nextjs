f744df19fd51973e90812f7bddcfc57f8d392f72
Bruno Guedão <brunoguedao@gmail.com>
2021-04-08 18:10:51 -0300

    new features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,41 @@
-export default function Home() {
+import { GetServerSideProps } from "next";
+import { Title } from "../styles/pages/Home";
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
+  const handleSum = async () => {
+    const math = await (await import("../lib/math")).default;
+    alert(math.sum(3, 5));
+  };
+
   return (
     <div>
-      <h1>Hello World!!!!</h1>
+      <section>
+        <Title>Products</Title>
+        <ul>
+          {recommendedProducts.map((recommendedProduct) => {
+            return (
+              <li key={recommendedProduct.id}>{recommendedProduct.title}</li>
+            );
+          })}
+        </ul>
+        <button onClick={handleSum}>Sum!</button>
+      </section>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch("http://localhost:3333/recommended");
+  const recommendedProducts = await response.json();
+  return {
+    props: { recommendedProducts },
+  };
+};
